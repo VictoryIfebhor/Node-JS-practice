3be45f4bfa296d65e09f01e0e3a64257153b593a
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-04-27 08:16:45 +0100

    Added connection to mongoDB

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,11 +1,21 @@
 const express = require("express")
-
+const connectDB = require("./db/connect")
 require("dotenv").config()
 
 const app = express()
 
 const port = process.env.PORT
+const mongoUrl = process.env.CONNECTION_URL
 
-app.listen(port, () => {
-    console.log(`Server listening to port ${port}...`)
-})
+const start = async () => {
+    try {
+        await connectDB(mongoUrl)
+
+        app.listen(port, () => console.log(`Server listening to port ${port}...`))
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
