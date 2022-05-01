bcc8e11f9d7ef719ea3d90a2b6014f97664e03cc
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-01 08:49:28 +0100

    Added validation details to the task schema

--- task-manager/models/task.js
+++ task-manager/models/task.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose")
 
 
 const TaskSchema = new mongoose.Schema({
-    name: String, completed: Boolean
+    name: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model("Task", TaskSchema)
