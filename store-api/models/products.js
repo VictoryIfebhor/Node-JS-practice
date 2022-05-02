f4cc91a833199710fc7d1e663d9a62c48c202cd7
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-02 15:04:58 +0100

    Created model for products

---
+++ store-api/models/products.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Product name is required"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+    },
+    rating: {
+        type: Number,
+        default: 0.0,
+    },
+    company: {
+        type: String,
+        enum: {
+            values: ["ikea", "liddy", "caressa", "marcos"],
+            message: "{VALUE} is not a supported"
+        }
+    },
+    feature: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+
+module.exports = mongoose.model("Product", ProductSchema)
