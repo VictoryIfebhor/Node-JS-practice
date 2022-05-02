0c88a0b47554d7a36dd464ae0c7a3f4da53c532e
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-02 10:55:59 +0100

    Added a middleware for all other routes that doesn't exist

--- task-manager/app.js
+++ task-manager/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const connectDB = require("./db/connect")
 const task = require("./routes/task")
 const errorHandlerMiddleware = require("./middleware/error-handler")
+const routeNotFound = require("./middleware/not-found")
 require("dotenv").config()
 
 const app = express()
@@ -12,6 +13,8 @@ const mongoUrl = process.env.CONNECTION_URL
 app.use(express.json())
 
 app.use("/api/v1/tasks", task)
+
+app.use(routeNotFound)
 app.use(errorHandlerMiddleware)
 
 const start = async () => {
