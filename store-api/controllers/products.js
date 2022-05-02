9198e1c0b0c8d1fdad0ae9bd1c80de2c5aa78d87
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-02 22:08:51 +0100

    Added options to filter results by

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,7 +1,25 @@
 const Product = require("../models/products")
+const { createCustomError } = require("../utils/exception")
 
 const getProducts = async (req, res) => {
-    const products = await Product.find()
+    const { name, company, featured } = req.query
+    const queryObject = {}
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" }
+    }
+    if (featured) {
+        if (["true", "false"].includes(featured)){
+            queryObject.featured = featured === "true" ? true : false
+        } else {
+            throw createCustomError(`"${featured}" is not a valid input for featured. "featured" can only be true or false`, 400)
+        }
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject)
     return res.json({ count: products.length, products })
 }
 
