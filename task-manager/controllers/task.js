0eb2aa8e5bf269293e3a747d660cfb4c286bc97a
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-02 01:49:17 +0100

    Added endpoint to get particular task by id

--- task-manager/controllers/task.js
+++ task-manager/controllers/task.js
@@ -1,5 +1,6 @@
 const Task = require("../models/task")
 const asyncWrapper = require("../middleware/async-wrapper")
+const { createCustomError } = require("../utils/exception")
 
 const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body)
@@ -11,8 +12,17 @@ const getAllTasks = asyncWrapper(async (_, res) => {
     return res.json(tasks)
 })
 
+const getSingleTask = asyncWrapper(async (req, res, next) => {
+    const task = await Task.findById(req.params.id)
+    if (!task){
+        console.log("No task found")
+        return next(createCustomError("Task does not exists", 400))
+    }
+    return res.json({ task })
+})
 
 module.exports = {
     createTask,
-    getAllTasks
+    getAllTasks,
+    getSingleTask
 }
--- task-manager/routes/task.js
+++ task-manager/routes/task.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const { createTask, getAllTasks } = require("../controllers/task")
+const { createTask, getAllTasks, getSingleTask } = require("../controllers/task")
 
 const router = express.Router()
 
 router.route("/").get(getAllTasks).post(createTask)
+router.route("/:id").get(getSingleTask)
 
 module.exports = router
