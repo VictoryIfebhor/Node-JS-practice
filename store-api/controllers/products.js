ebe974631f892bc8ad9da2239cff8da8ed13a5f2
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-04 02:31:43 +0100

    Added more query options to allow filtering of data

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/products")
 const { createCustomError } = require("../utils/exception")
 
 const getProducts = async (req, res) => {
-    const { name, company, featured } = req.query
+    const { name, company, featured, numericFilters } = req.query
     const queryObject = {}
 
     if (name) {
@@ -18,8 +18,38 @@ const getProducts = async (req, res) => {
     if (company) {
         queryObject.company = company
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject)
+    if (numericFilters) {
+        const operators = {
+            ">": "$gt",
+            ">=": "$gte",
+            "<": "$lt",
+            "<=": "$lte",
+            "=": "$eq"
+        }
+        const regex = /\b(>|>=|<|<=|=)\b/g
+        const filters = numericFilters.replace(regex, (match) => `--${operators[match]}--`)
+        filters.split(",").forEach(item => {
+            const [field, op, value] = item.split("--")
+            if (["price", "rating"].includes(field)) {
+                if (!(field in queryObject)) {
+                    queryObject[field] = {}
+                }
+                queryObject[field][op] = Number(value)
+            }
+        })
+    }
+    
+    let pQueryObject = Product.find(queryObject)
+
+    const sort = req.query.sort ? req.query.sort.split(",").join(" ") : "-rating"
+    const fields = req.query.fields ? req.query.fields.split(",").joint(" ") : ""
+    const limit = Number(req.query.limit) || 10
+    const page = Number(req.query.page) || 1
+    const skip = (page - 1) * limit
+    
+    pQueryObject = pQueryObject.sort(sort).select(fields).skip(skip).limit(limit)
+
+    const products = await pQueryObject
     return res.json({ count: products.length, products })
 }
 
