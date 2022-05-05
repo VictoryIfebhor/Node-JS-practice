7b4ae6e66bd4137b4417c7101ee0c98fcdfec40d
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-05 16:44:09 +0100

    Removed a line break. Found it to be excess

--- jobs-api/errors/exception.js
+++ jobs-api/errors/exception.js
@@ -14,7 +14,6 @@ const createCustomError = (message, statusCode) => {
 const invalidCredentials = createCustomError("Invalid Credentials", StatusCodes.UNAUTHORIZED)
 const unauthenticated = createCustomError("Not Authenticated", StatusCodes.UNAUTHORIZED)
 
-
 module.exports = {
     createCustomError,
     CustomHttpError,
