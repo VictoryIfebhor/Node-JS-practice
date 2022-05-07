1bea5741de42f6d5450716a4416dd4a39de9e1d6
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-07 02:21:39 +0100

    Imported StatusCodes

--- jobs-api/middlewares/auth.js
+++ jobs-api/middlewares/auth.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const jwt = require('jsonwebtoken')
+const { StatusCodes } = require("http-status-codes")
 
 const { createCustomError } = require("../errors/exception")
 
