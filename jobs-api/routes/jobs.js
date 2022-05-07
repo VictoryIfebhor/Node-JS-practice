425f8ba930657f8902479ccb79f466edb6e25e1b
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-07 04:18:42 +0100

    Nothing much. Just rearranged

--- jobs-api/routes/jobs.js
+++ jobs-api/routes/jobs.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const router = express.Router()
 
 const { getJobs, getJob, deleteJob, updateJob, createJob } = require("../controllers/jobs")
+
+const router = express.Router()
 
 router.route("/").get(getJobs).post(createJob)
 router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob)
