a7aa004c7ac76d22a7f6b0b8a2e03df1308bc6b7
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-07 06:22:39 +0100

    Added middlewares to handle security

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -1,7 +1,12 @@
 require("dotenv").config()
 require("express-async-errors")
 
-const express = require('express');
+const express = require('express')
+const rateLimit = require("express-rate-limit")
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean');
+
 
 const connectDB = require("./db")
 const authMiddleware = require("./middlewares/auth")
@@ -15,10 +20,22 @@ const users = require("./routes/user")
 
 const app = express()
 
+app.set("trust proxy", 1)
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 
+app.get("/", (req, res) => res.send(req.ip))
 app.use("/api/v1/auth", auth)
 
 app.use(authMiddleware)
