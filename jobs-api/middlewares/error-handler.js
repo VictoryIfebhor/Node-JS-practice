6ddef62e5407141ca153d89098c688762c748762
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-07 05:19:18 +0100

    Edited the error handler middleware

--- jobs-api/middlewares/error-handler.js
+++ jobs-api/middlewares/error-handler.js
@@ -7,12 +7,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         return res.status(err.statusCode).json({msg: err.message})
     }
     if (err instanceof ValidationError){
-        return res.status(400).json({msg: err.message})
+        const msg = Object.values(err.errors).map(item => item.message).join(", ")
+        return res.status(400).json({ msg })
     }
-    if (err instanceof CastError){
-        // Only the first 7 words should be sent. The rest contains information that shouldn't be sent.
-        errorMessage = err.message.split(" ").slice(0, 7).join(" ")
-        return res.status(400).json({msg: errorMessage})
+    if (err instanceof CastError && err.path === "_id"){
+
+        msg = `Record does not exist for id: ${err.value}`
+        return res.status(404).json({ msg })
+    }
+    if (err.code && err.code === 11000) {
+        return res.status(400).json({msg: `${Object.keys(err.keyValue)} already exists. Choose something else`})
     }
     return res.status(500).json({msg: "Internal Server Error"})
 }
