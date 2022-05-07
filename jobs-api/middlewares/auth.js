cab68b76ede36893007112d2a7b88785b5481a22
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-07 04:15:29 +0100

    Changed the way user is retrieved and passed to the next middleware

--- jobs-api/middlewares/auth.js
+++ jobs-api/middlewares/auth.js
@@ -3,11 +3,12 @@ require("dotenv").config()
 const jwt = require('jsonwebtoken')
 const { StatusCodes } = require("http-status-codes")
 
+const User = require("../models/user")
 const { createCustomError } = require("../errors/exception")
 
 const unauthenticated = createCustomError("Not Authenticated", StatusCodes.UNAUTHORIZED)
 
-const authMiddleware = (req, res,next) => {
+const authMiddleware = async (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         throw unauthenticated
@@ -15,7 +16,7 @@ const authMiddleware = (req, res,next) => {
     const token = authHeader.split(" ")[1]
     try{
         const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = { id: payload.sub }
+        req.user = await User.findById(payload.sub).select("-password")
         next()
     } catch (error) {
         throw unauthenticated
