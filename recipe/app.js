b2574dd4e39f390f30a6ec544aab2a6025bace05
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-11 14:34:33 +0100

    Protected the routes to home page and smoothies page

--- recipe/app.js
+++ recipe/app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 
 const connectDB = require("./db")
 const auth = require("./routes/auth")
+const pages = require("./routes/pages")
+const authMiddleware = require("./middlewares/authMiddleware")
 
 
 const app = express()
@@ -16,14 +18,13 @@ app.use(cookieParser())
 
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => res.render("home"))
-app.get("/smoothies", (req, res) => res.render("smoothies"))
 app.use("/users", auth)
+app.get("/:page", authMiddleware, pages)
 
 
 const start = async () => {
     try {
-        // await connectDB(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URI)
         app.listen(process.env.PORT, () => {
             console.log(`Server is listening on port ${process.env.PORT}`)
         })
