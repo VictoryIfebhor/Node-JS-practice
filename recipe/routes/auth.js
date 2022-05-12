27b25dac05884662cc4c8553513d88d4e7638635
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-12 14:43:55 +0100

    Added logout to the users route

--- recipe/routes/auth.js
+++ recipe/routes/auth.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
 
-const { signUpPage, signUpUser, loginPage, loginUser } = require("../controllers/auth")
+const { signOutUser, signUpPage, signUpUser, loginPage, loginUser } = require("../controllers/auth")
 
 const router = Router()
 
 router.route("/signup").get(signUpPage).post(signUpUser)
 router.route("/login").get(loginPage).post(loginUser)
+router.route("/logout").get(signOutUser)
 
 module.exports = router
