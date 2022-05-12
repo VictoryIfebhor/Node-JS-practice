89482d5ff85953bddc541620763ca7ccb66da02f
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-12 14:40:42 +0100

    Made changes to the error middleware to also send server error

--- recipe/middlewares/errorHandlerMiddleware.js
+++ recipe/middlewares/errorHandlerMiddleware.js
@@ -2,19 +2,22 @@ const { Error: { ValidationError } } = require("mongoose")
 const { CustomFieldError } = require("../errors")
 
 const errorHandler = (error, req, res, next) => {
+    console.log(error)
     let errors = {}
     if (error instanceof CustomFieldError) {
         errors[error.field] = error.message
+        return res.status(400).json({ errors })
     }
-    else if (error.code === 11000){
+    if (error.code === 11000) {
         errors = {email: "Email already registered"}
-    } else if (error instanceof ValidationError) {
+        return res.status(400).json({ errors })
+    } if (error instanceof ValidationError) {
         Object.values(error.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message
         })
+        return res.status(400).json({ errors })
     }
-    
-    return res.status(400).json({ errors })
+    return res.status(500).json({ errors: { general: "Internal Server Error. Try again later or contact developer"} })
 }
 
 module.exports = errorHandler
