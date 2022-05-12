3126c3013cad24f3790183d817e64b38642bab43
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-05-12 14:44:40 +0100

    Added a div to display general error messges

--- recipe/public/submitForm.js
+++ recipe/public/submitForm.js
@@ -2,15 +2,17 @@ const form = document.querySelector("form")
 const submitBtn = document.querySelector("form > button")
 const emailError = document.querySelector(".email.error")
 const passwordError = document.querySelector(".password.error")
+const generalError = document.querySelector(".general.error")
 form.addEventListener("submit", async (e) => {
     e.preventDefault()
     // reset error messages if it exists
     emailError.textContent = ""
     passwordError.textContent = ""
+    generalError.textContent = ""
 
     const email = form.email.value
     const password = form.password.value
-    const btnValue = submitBtn.value.toLowerCase()
+    const btnValue = submitBtn.innerHTML.toLowerCase()
 
     if (!["login", "sign up"].includes(btnValue)){
         console.log(`No action was designated ${btnValue} button`)
@@ -26,6 +28,7 @@ form.addEventListener("submit", async (e) => {
             const data = await res.json()
             emailError.textContent = data.errors?.email || ""
             passwordError.textContent = data.errors?.password || ""
+            generalError.textContent = data.errors?.general || ""
             if (data.user) {
                 location.assign("/")
             }
